1517d38eb69a400513c5b58e6326c426848ab0c3
hailstony <hzxxmy@gmail.com>
2017-02-21 12:35:37 -0800

    basically done with stmt

--- ast_stmt.cc
+++ ast_stmt.cc
@@ -53,6 +53,27 @@ void StmtBlock::PrintChildren(int indentLevel) {
     stmts->PrintAll(indentLevel+1);
 }
 
+void StmtBlock::Check(){
+    //Statement block does not need to push a new scope, the statement
+    //before it (if, funcdecl, etc) should do the job
+    if(decls){
+        //if decls is not NULL pointer, check every decl
+        int size = decls->NumElements();
+        for(int i = 0; i < size; i++){
+            VarDecl * element = decls->Nth(i);
+            element->Check();
+        }
+    }
+    if(stmts){
+        //if statement list is not NULL pointer, check every stmt
+        int size = stmts->NumElements();
+        for(int i = 0; i < size; i++){
+            Stmt * element = stmts->Nth(i);
+            element->Check();
+        }
+    }
+}
+
 DeclStmt::DeclStmt(Decl *d) {
     Assert(d != NULL);
     (decl=d)->SetParent(this);
@@ -60,6 +81,12 @@ DeclStmt::DeclStmt(Decl *d) {
 
 void DeclStmt::PrintChildren(int indentLevel) {
     decl->Print(indentLevel+1);
+}
+
+void DeclStmt::Check(){
+    //not sure if we need to dynamic cast decl to vardecl & fndecl
+    if(decl)
+        decl->Check();
 }
 
 ConditionalStmt::ConditionalStmt(Expr *t, Stmt *b) {
@@ -84,9 +111,54 @@ void ForStmt::PrintChildren(int indentLevel) {
     body->Print(indentLevel+1, "(body) ");
 }
 
+void ForStmt::Check(){
+    //p3exe will let it pass as long as init, body are valid expr,
+    //step has to be boolean
+    if(init){
+        init->Check();
+    }
+    if(step){
+        step->Check();
+    }
+    //test has to be a valid pointer enforced by parser
+    if(test){
+        test->Check();
+        Type * testType = test->GetType();
+        if(!testType->IsEquivalentTo(Type::boolType)){
+            ReportError::TestNotBoolean(test);
+        }
+    }
+    //should push a new scope for the following statement body block
+    if(body){
+        Node::symtable->push();
+        Node::loopNum++;
+        body->Check();
+        Node::loopNum--;
+        Node::symtable->pop();
+    }
+}
+
 void WhileStmt::PrintChildren(int indentLevel) {
     test->Print(indentLevel+1, "(test) ");
     body->Print(indentLevel+1, "(body) ");
+}
+
+boid WhileStmt::Check(){
+    if(test){
+        test->Check();
+        Type * testType = test->GetType();
+        if(!testType->IsEquivalentTo(Type::boolType)){
+            ReportError::TestNotBoolean(test);
+        }
+    }
+    //should push a new scope for the following statement body block
+    if(body){
+        Node::symtable->push();
+        Node::loopNum++;
+        body->Check();
+        Node::loopNum--;
+        Node::symtable->pop();
+    }
 }
 
 IfStmt::IfStmt(Expr *t, Stmt *tb, Stmt *eb): ConditionalStmt(t, tb) {
@@ -101,6 +173,28 @@ void IfStmt::PrintChildren(int indentLevel) {
     if (elseBody) elseBody->Print(indentLevel+1, "(else) ");
 }
 
+void IfStmt::Check(){
+    if(test){
+        test->Check();
+        Type * testType = test->GetType();
+        if(!testType->IsEquivalentTo(Type::boolType)){
+            ReportError::TestNotBoolean(test);
+        }
+    }
+    //should push a new scope for the following statement body block
+    if(body){
+        Node::symtable->push();
+        body->Check();
+        Node::symtable->pop();
+    }
+
+    //push another scope for else body
+    if(elseBody){
+        Node::symtable->push();
+        elseBody->Check();
+        Node::symtable->pop();
+    }
+}
 
 ReturnStmt::ReturnStmt(yyltype loc, Expr *e) : Stmt(loc) {
     expr = e;
@@ -110,6 +204,39 @@ ReturnStmt::ReturnStmt(yyltype loc, Expr *e) : Stmt(loc) {
 void ReturnStmt::PrintChildren(int indentLevel) {
     if ( expr )
       expr->Print(indentLevel+1);
+}
+
+void ReturnStmt::Check(){
+    //set hasReturn to true if actually return something
+    //p3exe will not report missing return as long as there is a return
+    Node::symtable->hasReturn = true;
+
+    //case 1: return nothing but function requres something
+    if(expr == NULL && Node::symtable->needReturn == true)
+        rReportError::ReturnMismatch(this, Type::voidType, Node::symtalbe->needReturnType);
+    
+    expr->Check();
+    Type * returnType = expr->GetType();
+    
+    //case 2: return something but function requires nothing
+    if(Node::symtable->needReturn == false && !returnType->IsEquivalentTo(Type::voidType)){
+        ReportError::ReturnMismatch(this, returnType, Type::voidType);
+    }else{
+        //case3: return something function requires something else
+        if(!returnType->IsEquivalentTo(Node::symtable->needReturnType)){
+            ReportError::ReturnMismatch(this, returnType, Node::symtalbe->needReturnType);
+        }
+    }
+}
+
+void BreakStmt::Check(){
+    if(Node::symtable->loopNum == 0 && Node::symtable->switchNum == 0)
+        ReportError::BreakOutsideLoop(this);
+}
+
+void ContinueStmt::Check(){
+    if(Node::symtable->loopNum == 0)
+        ReportError::ContinueOutsideLoop(this);
 }
 
 SwitchLabel::SwitchLabel(Expr *l, Stmt *s) {
@@ -127,6 +254,44 @@ SwitchLabel::SwitchLabel(Stmt *s) {
 void SwitchLabel::PrintChildren(int indentLevel) {
     if (label) label->Print(indentLevel+1);
     if (stmt)  stmt->Print(indentLevel+1);
+}
+
+void SwitchLabel::Check(){
+    //SwitchLabel constructor is never called in parser
+}
+
+void SwitchStmt::Check(){
+    if(expr){
+        expr->Check();
+    }
+    Node::symtable->push();
+    Node::symtable->switchNum++;
+    if(def){
+        def->Check();
+    }
+    if(cases){
+        int size = cases->NumElements();
+        for(int i = 0; i < size; i++){
+            Stmt * element = cases->Nth(i);
+            element->Check();
+        }
+    }
+    Node::symtable->switchNum--;
+    Node::symtable->pop();
+}
+
+void Case::Check(){
+    if(label)
+        label->Check();
+    if(stmt)
+        stmt->Check();
+}
+
+void Default::Check(){
+    if(label)
+        label->Check();
+    if(stmt)
+        stmt->Check();
 }
 
 SwitchStmt::SwitchStmt(Expr *e, List<Stmt *> *c, Default *d) {
